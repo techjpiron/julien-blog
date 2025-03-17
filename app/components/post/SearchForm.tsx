7ca1c446c2e8d645c62f2fbcc942f894b6401cfd
Julien Piron <tech@jpiron.eu>
2025-03-17 09:26:37 +0100

    feat: add loading spinner to post filter

--- app/components/post/SearchForm.tsx
+++ app/components/post/SearchForm.tsx
@@ -1,5 +1,5 @@
 import { SearchField } from "react-aria-components";
-import { Form, useSearchParams, useSubmit } from "react-router";
+import { Form, useNavigation, useSearchParams, useSubmit } from "react-router";
 import { Input } from "~/components/ui/Field";
 import { Button } from "~/components/ui/Button";
 import { useRef } from "react";
@@ -8,6 +8,13 @@ export function SearchForm() {
   const [params] = useSearchParams();
   const query = params.get("q") ?? "";
 
+  const navigation = useNavigation();
+  const searching =
+    navigation.location &&
+    new URLSearchParams(navigation.location.search).has("q");
+  const isFirstSearch = query.length == 0;
+  console.log({ isFirstSearch });
+
   const submit = useSubmit();
   const formRef = useRef<HTMLFormElement>(null);
 
@@ -15,17 +22,24 @@ export function SearchForm() {
     <Form
       className="mt-4"
       ref={formRef}
-      onChange={(event) => submit(event.currentTarget)}
+      onChange={(event) =>
+        submit(event.currentTarget, { replace: !isFirstSearch })
+      }
     >
       <SearchField
         className="flex"
         name="q"
         aria-label="Search posts"
-        defaultValue={query}
-        onClear={() => submit(formRef.current?.target ?? {})}
-        onChange={() => submit(formRef.current?.target ?? {})}
+        value={query}
+        onClear={() =>
+          submit(formRef.current?.target ?? {}, { replace: !isFirstSearch })
+        }
+        onChange={() =>
+          submit(formRef.current?.target ?? {}, { replace: !isFirstSearch })
+        }
       >
         <Input placeholder="Search" />
+        <span aria-hidden>{searching ? "Searching" : null}</span>
         <Button>X</Button>
       </SearchField>
       <input type="hidden" name="p" value="1" />
