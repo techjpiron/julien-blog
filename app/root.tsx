8d7467fefc195c20087486100004a1962236c62e
Julien Piron <tech@jpiron.eu>
2025-03-18 08:53:37 +0100

    feat: improve error pages

--- app/root.tsx
+++ app/root.tsx
@@ -1,5 +1,4 @@
 import {
-  href,
   isRouteErrorResponse,
   Links,
   Meta,
@@ -9,7 +8,7 @@ import {
 } from "react-router";
 import type { Route } from "./+types/root";
 import "~/app.css";
-import { H1, Link, P } from "~/components/ui/Typography";
+import { H1, P } from "~/components/ui/Typography";
 
 export const links: Route.LinksFunction = () => [
   { rel: "preconnect", href: "https://fonts.googleapis.com" },
@@ -25,14 +24,14 @@ export const links: Route.LinksFunction = () => [
 ];
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+    <html lang="en" className="h-full">
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta />
         <Links />
       </head>
-      <body className="font-body antialiased selection:bg-pink-300">
+      <body className="h-full font-body  antialiased selection:bg-pink-300">
         {children}
         <ScrollRestoration />
         <Scripts />
@@ -62,21 +61,19 @@ export function ErrorBoundary({ error }: Route.ErrorBoundaryProps) {
   }
 
   return (
-    <main className="container mx-auto p-4 pt-16">
-      <H1>{message}</H1>
-      <P className="text-xl">{details}</P>
-      {stack && (
-        <pre className="w-full overflow-x-auto p-4">
-          <code>{stack}</code>
-        </pre>
-      )}
-      <Link
-        to={href("/")}
-        onClick={() => window.location.reload()}
-        className="mt-4"
-      >
-        Refresh page
-      </Link>
+    <main className="h-full mx-auto max-w-3/4 px-4 flex items-center justify-center">
+      <div>
+        <H1>{message}</H1>
+        <P className="text-xl">{details}</P>
+        {stack && (
+          <pre className="w-full overflow-x-auto p-4">
+            <code>{stack}</code>
+          </pre>
+        )}
+        <a href="/" className="mt-4">
+          Go to homepage
+        </a>
+      </div>
     </main>
   );
 }
